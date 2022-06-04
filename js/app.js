09668988c4a1038d880f05315b82197674d807cf
Bouna Mohamed Mahfoudh <83521427+medmahfoudh@users.noreply.github.com>
2022-06-04 14:23:09 +0100

    js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
